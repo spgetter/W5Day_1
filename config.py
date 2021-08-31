8413901b8f641a8fea592a98306933fd5823857b
spgetter <spgetter@gmail.com>
2021-08-30 20:48:08 -0600

    W5Day_1HW Commit

---
+++ config.py
@@ -0,0 +1,18 @@
+import os
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+# Give access to the project in ANY os we find oursleves in
+# Allow outside files/folders to be added to the project from the 
+# Base directory
+
+class Config():
+    """
+        Set Config variables for the Flask app
+        Using Environment varialbles where available
+        Otherwise create the config variables if not done already.
+    """
+
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'You will probably guess'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False # Turn off Update Messages from the sqlalchemy
